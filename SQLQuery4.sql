b52a1ecf9b8f876db1942a4f9423bc944dec0e3e
Jonás Javier Giménez <jonasgimenez1996@gmail.com>
2025-08-01 16:11:14 -0300

    Delete SQLQuery4.sql
    Eliminado script de Ejercicio 4 (con error de consigna)

--- SQLQuery4.sql
+++
@@ -1,8 +0,0 @@
-
--- EJERCICIO 2: Cantidad de productos distintos por factura.
-
-select count(id_producto) as "cantidad de productos"
-from detalle
-where num_factura = 1001
-select * 
-from detalle
